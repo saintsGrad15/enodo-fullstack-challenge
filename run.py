d9002b21a5901f656098e1560720f795c0cd8f00
John Carrell <carrell.john@gmail.com>
2020-07-30 09:44:06 -0500

    Tell application where the static directory is

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 configure_logging()
 
-flask_application = Flask("enodo")
+flask_application = Flask("enodo", static_folder="app/static")
 
 create_database()
 
